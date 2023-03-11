da3a312d197d79f2c25c146dbd3cf89547f5193a
Ca11m3X <adamtrinh1711@gmail.com>
2023-03-11 17:19:17 +0700

    pnp_3_custom

--- scripts/pick_and_place.py
+++ scripts/pick_and_place.py
@@ -14,7 +14,7 @@ def get_object_info():
     z = float(input("object_distance_z:= "))
     point = (x,y,z)
 
-    print("___Release Position___")
+    print("\n___Release Position___")
     x_f = float(input("distance_x:= "))
     y_f = float(input("distance_y:= "))
     z_f = float(input("distance_z:= "))
@@ -25,27 +25,60 @@ if __name__ == "__main__":
     bot = InterbotixManipulatorXS("rx150", "arm", "gripper")
     finish = True
     while finish:
-        A, B = get_object_info()
+        # A, B = get_object_info()
+        safety_height = 4*0.01
+        object_height = 2.5*0.01 
+        # A = (0.2, 0, -5*0.01)
+        # A = (0.15, 0.05, -5*0.01)
+        # A = (0.1, -0.2, -5*0.01)
+        A= [(0.2, 0, -5*0.01), (0.15, 0.05, -5*0.01), (0.13, -0.08, -5*0.01)]
+        # print(type(A))
+        B = (0, 0.2, 0.02)
+        for i in range(3):
+            bot.arm.go_to_home_pose()
+            bot.arm.set_ee_pose_components(x= A[i][0], y= A[i][1], z= A[i][2]+object_height+safety_height,pitch= 1.57)
+            angle = bot.arm.get_single_joint_command('waist')
+            bot.arm.set_single_joint_position('wrist_rotate', angle)
+            bot.gripper.open()
+            bot.arm.set_ee_cartesian_trajectory(z=-safety_height+0.01)
+            bot.gripper.close()
+            bot.arm.set_ee_cartesian_trajectory(z=+safety_height-0.01+(B[2]-A[i][2]))
+            bot.arm.set_ee_pose_components(x= B[0], y= B[1], z= B[2]+object_height+safety_height, pitch= 1.57)
+            bot.gripper.open()
+            
         bot.arm.go_to_home_pose()
-        input()
-        bot.arm.set_ee_pose_components(x= A[0], y= A[1], z= A[2]+0.08, pitch= 1.57)
-        input()
-        bot.gripper.open()
-        bot.arm.set_ee_cartesian_trajectory(z=-0.08)
-        bot.gripper.close()
-        bot.arm.set_ee_cartesian_trajectory(z=0.08)
-
-        bot.arm.set_ee_pose_components(x= B[0], y= B[1], z= B[2]+0.08, pitch= 1.57)
-        bot.arm.set_ee_cartesian_trajectory(z=-0.08)
-        bot.gripper.open()
-        bot.arm.set_ee_cartesian_trajectory(z=0.08)
-
-        bot.arm.go_to_home_pose()
-
         go_to_sleep_pose()
 
-        check = 'a'
-        check = input("Repeat?(Y/N)\n")
-        if check.upper() == 'N':
-            finish = False
-    
+        check = ''
+        while check != 'Y' and check != 'N':
+            check = input("Continue with custom object position?(Y/N)\n").upper()
+            if check.upper() == 'N':
+                finish = False
+
+        if finish:
+            finish2 = True
+            while finish2:
+                A, B = get_object_info()
+                bot.arm.go_to_home_pose()
+                bot.arm.set_ee_pose_components(x= A[0], y= A[1], z= A[2]+object_height+safety_height,pitch= 1.57)
+                angle = bot.arm.get_single_joint_command('waist')
+                bot.arm.set_single_joint_position('wrist_rotate', angle)
+                bot.gripper.open()
+                bot.arm.set_ee_cartesian_trajectory(z=-safety_height+0.02)
+                bot.gripper.close()
+                bot.arm.set_ee_cartesian_trajectory(z=+safety_height-0.02+(B[2]-A[2]))
+                bot.arm.set_ee_pose_components(x= B[0], y= B[1], z= B[2]+object_height+safety_height, pitch= 1.57)
+                bot.gripper.open()
+                bot.arm.go_to_home_pose()
+                go_to_sleep_pose()
+                check = ''
+                while check != 'Y' and check != 'N':
+                    check = input("Keep on custom object position mode?(Y/N)\n").upper()
+                    if check.upper() == 'N':
+                        finish2 = False
+
+        check = ''
+        while check != 'Y' and check != 'N':
+            check = input("Exit?(Y/N)\n").upper()
+            if check.upper() == 'Y':
+                finish = False
